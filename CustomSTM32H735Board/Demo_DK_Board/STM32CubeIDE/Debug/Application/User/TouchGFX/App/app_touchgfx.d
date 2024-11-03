831f1a7776da1c1468d94f9e23904b8c39b24ca8
Stefan Meyre <stefan@hb9gze.ch>
2024-11-03 16:04:48 +0100

    stuff that I can not remember

---
+++ CustomSTM32H735Board/Demo_DK_Board/STM32CubeIDE/Debug/Application/User/TouchGFX/App/app_touchgfx.d
@@ -0,0 +1,4 @@
+Application/User/TouchGFX/App/app_touchgfx.o: \
+ C:/ProjectsOnC/PhasorRadio/CustomSTM32H735Board/Demo_DK_Board/TouchGFX/App/app_touchgfx.c \
+ C:/ProjectsOnC/PhasorRadio/CustomSTM32H735Board/Demo_DK_Board/TouchGFX/App/app_touchgfx.h
+C:/ProjectsOnC/PhasorRadio/CustomSTM32H735Board/Demo_DK_Board/TouchGFX/App/app_touchgfx.h:
